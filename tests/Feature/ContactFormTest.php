c511bafe30765e2041b7fbb8cc6cc352f7787532
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-09-27 13:14:19 +0630

    check contact form submit successfully

--- tests/Feature/ContactFormTest.php
+++ tests/Feature/ContactFormTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Livewire\Livewire;
+use App\Http\Livewire\ContactForm;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ContactFormTest extends TestCase
 {
@@ -17,5 +19,16 @@ class ContactFormTest extends TestCase
     {
         $this->get('/contact_form')
                 ->assertSeeLivewire('contact-form');
+    }
+
+    public function testContactFormSendOutAnEmail()
+    {
+        Livewire::test(ContactForm::class)
+                    ->set('name', 'Htoo Maung Thait')
+                    ->set('email', 'htoo.mt@blueplanet.com.mm')
+                    ->set('subject', 'Testing')
+                    ->set('message', 'This is a testing message')
+                    ->call('submitForm')
+                    ->assertSee('Your email has been sent successfully!');
     }
 }
