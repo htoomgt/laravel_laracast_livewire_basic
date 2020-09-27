c0eb6e59178ed737d36b30957f2cde43a699335c
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-09-27 18:53:31 +0630

    datatable feature initial commit

--- app/Http/Livewire/DataTables.php
+++ app/Http/Livewire/DataTables.php
@@ -11,10 +11,28 @@ class DataTables extends Component
     use WithPagination;
 
     public $search;
+    public $active = true;
+    public $sortField;
+    public $sorAsc = true;
+    protected $queryString = ['search', 'active', 'sortAsc', 'sortField'];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
 
     public function render()
     {
-        return view('livewire.data-tables', ['users' => User::paginate(10)]);
+        return view('livewire.data-tables', [
+            'users' => User::paginate(10),
+            'users' => User::where(function($query){
+                            $query->where('name', 'like', '%'.$this->search.'%')
+                            ->orWhere('email', 'like', '%'.$this->search.'%');
+                        })->where('active', $this->active)
+                        ->where($this->sortField, function($query){
+                            $query->orderBy($this->sortField, $this->sorAsc ? 'asc' : 'desc');
+                        })->paginate(10),
+        ]);
     }
 
    /*  public function paginationView()
