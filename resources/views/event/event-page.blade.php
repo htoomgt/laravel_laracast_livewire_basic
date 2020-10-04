51f9a022eb397732d2762ee1f4080af665eced23
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-10-05 04:06:04 +0630

    event with tags feature initial commit

---
+++ resources/views/event/event-page.blade.php
@@ -0,0 +1,10 @@
+@include('layouts.header')
+    <div class="my-8">
+        <h2 class="text-lg font-semibold mt-4"> Events Example with Tags</h2>
+
+        <div class="mt-4">
+
+        </div>
+    </div>
+
+@include('layouts.footer')
