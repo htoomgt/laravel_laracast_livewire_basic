a4d4e29dfd4becb81d271211d7c4f546bd306c2f
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-10-04 13:43:24 +0630

    poll feature initial commit

---
+++ app/Http/Livewire/PollExample.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PollExample extends Component
+{
+    public function render()
+    {
+        return view('livewire.poll-example');
+    }
+}
